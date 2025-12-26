894c059061ba749880b5258a67a9e68bab0e8837
suryansh1440 <suryansh1440@gmail.com>
2025-12-26 23:45:57 +0530

    minnor changes

--- client/src/api/videoApi.js
+++ client/src/api/videoApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1/video';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://3.84.16.73/api/v1/video';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
